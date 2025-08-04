4142130d2523cddfbabafd1bc7019bebeb08c404
Tab-0verload <toomuchtab@gmail.com>
2025-08-04 21:35:38 +0300

    Update shop.js

--- 2game/shop.js
+++ 2game/shop.js
@@ -25,7 +25,7 @@ export function render(container) {
   const clickCost = clickLevel * 50000;
   const clickBtn = document.createElement('button');
   clickBtn.classList.add('mclickgs');
-  clickBtn.innerHTML = `Клик <br> (x${clickLevel}, ${formatNumber(clickCost)})`;
+  clickBtn.innerHTML = `Клик <br> (x${formatNumber(clickLevel)}, ${formatNumber(clickCost)})`;
   clickBtn.onclick = () => {
     if (subtractBalance(clickCost)) {
       clickLevel++;
@@ -40,7 +40,7 @@ export function render(container) {
   const bankCost = bankMultiplier * 5000;
   const bankBtn = document.createElement('button');
   bankBtn.classList.add('mclickgs');
-  bankBtn.innerHTML = `% банка <br> (x${bankMultiplier}, ${formatNumber(bankCost)})`;
+  bankBtn.innerHTML = `% банка <br> (x${formatNumber(bankMultiplier)} | ${formatNumber(bankCost)})`;
   bankBtn.onclick = () => {
     if (subtractBalance(bankCost)) {
       bankMultiplier++;
@@ -55,7 +55,7 @@ export function render(container) {
   const depositCost = depositLevel * 500;
   const depositBtn = document.createElement('button');
   depositBtn.classList.add('mclickgs');
-  depositBtn.innerHTML = `Вклады <br> (уровень ${depositLevel}, ${formatNumber(depositCost)})`;
+  depositBtn.innerHTML = `Вклады <br> (уровень ${formatNumber(depositLevel)} | ${formatNumber(depositCost)})`;
   depositBtn.onclick = () => {
     if (subtractBalance(depositCost)) {
       depositLevel++;
