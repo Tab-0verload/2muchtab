23c49d01b85a7ef7c47b9c46c113a5c327d291b4
Tab-0verload <toomuchtab@gmail.com>
2025-08-15 01:32:31 +0300

    Update shop.js

--- 2game/shop.js
+++ 2game/shop.js
@@ -22,7 +22,7 @@ export function render(container) {
   const info = document.createElement('div');
 
   // Клик улучшение
-  const clickCost = clickLevel * 50000;
+  const clickCost = clickLevel * 100000;
   const clickBtn = document.createElement('button');
   clickBtn.classList.add('mclickgs');
   clickBtn.innerHTML = `Клик <br> (x${formatNumber(clickLevel)} | ${formatNumber(clickCost)})`;
@@ -37,7 +37,7 @@ export function render(container) {
   };
 
   // Множитель банка
-  const bankCost = bankMultiplier * 5000;
+  const bankCost = bankMultiplier * 1000;
   const bankBtn = document.createElement('button');
   bankBtn.classList.add('mclickgs');
   bankBtn.innerHTML = `% банка <br> (x${formatNumber(bankMultiplier)} | ${formatNumber(bankCost)})`;
